cb84ab369cce934a860785404f2edaf27386cc24
Shaheer Imran <shaheerimran@Shaheers-MacBook-Air.local>
2026-04-16 15:29:45 +0500

    Initial project setup with Spring Boot, MongoDB, JWT dependencies

---
+++ src/main/java/com/ramexchange/ramexchange/RamExchangeApplication.java
@@ -0,0 +1,12 @@
+package com.ramexchange.ramexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamExchangeApplication.class, args);
+    }
+}
